c75b8977f9fd3490f0d57f4f1490186c7891cd14
adelgado <adelgado0723@gmail.com>
2025-03-19 18:35:51 -0400

    sources env vars from script

--- scripts/backup_n8n.sh
+++ scripts/backup_n8n.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ../.s
 export ANSIBLE_CONFIG=../n8n/ansible/ansible.cfg
+
 echo "🚀 Running Ansible playbook..."
 ansible-playbook -i inventory ../n8n/ansible/playbooks/backup_n8n.yml
