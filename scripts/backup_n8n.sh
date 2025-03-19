b387e32021b3ae371d67f1f986cc42ebb933acbe
adelgado <adelgado0723@gmail.com>
2025-03-19 18:43:17 -0400

    moves to the correct directory for executing the backup script

--- scripts/backup_n8n.sh
+++ scripts/backup_n8n.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+dir=$PWD
+
+cd $HOME/serverSetup/scripts/
+
 source ../.s
 export ANSIBLE_CONFIG=../n8n/ansible/ansible.cfg
 
 echo "🚀 Running Ansible playbook..."
 ansible-playbook -i inventory ../n8n/ansible/playbooks/backup_n8n.yml
+
+if [ $? -ne 0 ]; then
+    echo "❌ Ansible playbook failed"
+fi
+
+cd $dir
