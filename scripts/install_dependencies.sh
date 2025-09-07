083b62b515c422151d2209db6944b9701b373f7b
adelgado <adelgado0723@gmail.com>
2025-09-07 18:27:05 -0400

    updates email and github user name

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,4 +3,4 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install ansible
-git clone https://github.com/adelgado0723/smsolutions_home.git ~/smsolutions_home/
+git clone https://github.com/swamp-dev/smsolutions_home.git ~/smsolutions_home/
